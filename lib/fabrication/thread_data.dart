ca7d2bd17358b559c35fb9022bcdfa0d4bdc5292
JvSlice <outeredgeforge@gmail.com>
2026-02-23 15:59:44 -0600

    Update thread_data.dart
    Added in common thread types for UNC, UNF, and Metric

--- lib/fabrication/thread_data.dart
+++ lib/fabrication/thread_data.dart
@@ -26,6 +26,13 @@ const List<ThreadSpec> commonUNC = [
     major: 0.1380,
     pitchOrTpi: 32,
   ),
+  ThreadSpec(system: ThreadSystem.unc, label: "#8-32",  major: 0.1640, pitchOrTpi: 32),
+  ThreadSpec(system: ThreadSystem.unc, label: "#10-24", major: 0.1900, pitchOrTpi: 24),
+  ThreadSpec(system: ThreadSystem.unc, label: "#10-32", major: 0.1900, pitchOrTpi: 32),
+  ThreadSpec(system: ThreadSystem.unc, label: "1/4-20", major: 0.2500, pitchOrTpi: 20),
+  ThreadSpec(system: ThreadSystem.unc, label: "5/16-18",major: 0.3125, pitchOrTpi: 18),
+  ThreadSpec(system: ThreadSystem.unc, label: "3/8-16", major: 0.3750, pitchOrTpi: 16),
+  ThreadSpec(system: ThreadSystem.unc, label: "1/2-13", major: 0.5000, pitchOrTpi: 13),
 ];
 const List<ThreadSpec> commonUNF = [
   ThreadSpec(
@@ -34,6 +41,14 @@ const List<ThreadSpec> commonUNF = [
     major: 0.138,
     pitchOrTpi: 40,
   ),
+   ThreadSpec(system: ThreadSystem.unf, label: "#8-36",   major: 0.1640, pitchOrTpi: 36),
+  ThreadSpec(system: ThreadSystem.unf, label: "#10-32",  major: 0.1900, pitchOrTpi: 32),
+  ThreadSpec(system: ThreadSystem.unf, label: "1/4-28",  major: 0.2500, pitchOrTpi: 28),
+  ThreadSpec(system: ThreadSystem.unf, label: "5/16-24", major: 0.3125, pitchOrTpi: 24),
+  ThreadSpec(system: ThreadSystem.unf, label: "3/8-24",  major: 0.3750, pitchOrTpi: 24),
+  ThreadSpec(system: ThreadSystem.unf, label: "1/2-20",  major: 0.5000, pitchOrTpi: 20),
+  
+  
 ];
 
 const List<ThreadSpec> commonMetric = [
@@ -43,4 +58,11 @@ const List<ThreadSpec> commonMetric = [
     major: 3.0,
     pitchOrTpi: 0.5,
   ),
+    ThreadSpec(system: ThreadSystem.metric, label: "M3 x 0.5",   major: 3.0,  pitchOrTpi: 0.5),
+  ThreadSpec(system: ThreadSystem.metric, label: "M4 x 0.7",   major: 4.0,  pitchOrTpi: 0.7),
+  ThreadSpec(system: ThreadSystem.metric, label: "M5 x 0.8",   major: 5.0,  pitchOrTpi: 0.8),
+  ThreadSpec(system: ThreadSystem.metric, label: "M6 x 1.0",   major: 6.0,  pitchOrTpi: 1.0),
+  ThreadSpec(system: ThreadSystem.metric, label: "M8 x 1.25",  major: 8.0,  pitchOrTpi: 1.25),
+  ThreadSpec(system: ThreadSystem.metric, label: "M10 x 1.5",  major: 10.0, pitchOrTpi: 1.5),
+  ThreadSpec(system: ThreadSystem.metric, label: "M12 x 1.75", major: 12.0, pitchOrTpi: 1.75),
 ];
