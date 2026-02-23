c596ef545c2e52a2057c36e6ca3d6019fa493d06
Jordan VanSickle <outeredgeforge@gmail.com>
2026-02-23 06:01:34 -0600

    app_theme created but unfinished

---
+++ lib/app_theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum AccentChoice { green, red, yellow }
+String accentChoiceToString(AccentChoice c) => c.name;
+
+Color accentColorFromChoice(AccentChoice c) {
+  switch (c) {
+    case AccentChoice.green:
+      return const Color(0xFE00E676);
+    case AccentChoice.red:
+      return const Color(0xFFFF5252);
+    case AccentChoice.yellow:
+      return const Color(0xFFFFD54F);
+  }
+}
+
+class AppThemeController extends ChangeNotifier {
+  static const _prefsKey = 'accent_choice';
+
+  AccentChoice _accentChoice = AccentChoice.green;
+  AccentChoice get AccentChoice => _accentChoice;
+
+  Color get AccentChoice => accentColorFromChoice(_accentChoice);
+
+  Future<void> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_prefsKey, accentChoiceToString(choice));
+  }
+}
+
+ThemeData buildTermialTheme(Color accent) {
+  final scheme = ColorScheme.fromSeed(
+    seedColor: accent,
+    brightness: Brightness.dark,
+  );
+  return ThemeData(
+    brightness: Brightness.dark
+    colorScheme: scheme,
+    scaffoldBackgroundColor: const Color(0xFF0BF14),
+    useMaterial3: true,
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(fontSize: 16)
+    ),
+  );
+}
--- lib/fabrication/thread_data.dart
+++ lib/fabrication/thread_data.dart
@@ -19,6 +19,28 @@ class ThreadSpec {
   });
 }
 
-const List<ThreadSpec> commonUNC = [];
-const List<ThreadSpec> commonUNF = [];
-const List<ThreadSpec> commonMetric = [];
+const List<ThreadSpec> commonUNC = [
+  ThreadSpec(
+    system: ThreadSystem.unc,
+    label: "#6-32",
+    major: 0.1380,
+    pitchOrTpi: 32,
+  ),
+];
+const List<ThreadSpec> commonUNF = [
+  ThreadSpec(
+    system: ThreadSystem.unf,
+    label: "#6-40",
+    major: 0.138,
+    pitchOrTpi: 40,
+  ),
+];
+
+const List<ThreadSpec> commonMetric = [
+  ThreadSpec(
+    system: ThreadSystem.metric,
+    label: "M3 x 0.0=5",
+    major: 3.0,
+    pitchOrTpi: 0.5,
+  ),
+];
