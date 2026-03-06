99772a10b7e5c8e2c46fc4da3e03676c2828a876
JvSlice <outeredgeforge@gmail.com>
2026-03-05 20:22:51 -0600

    Update material_units.dart

---
+++ lib/reference/materials/material_units.dart
@@ -0,0 +1,5 @@
+double kgm3ToLbIn3(double kgm3) => kgm3 * 0.0000361273;  // 1 kg/m³ = 3.61273e-5 lb/in³
+double kgm3ToLbFt3(double kgm3) => kgm3 * 0.06242796;    // 1 kg/m³ = 0.06242796 lb/ft³
+
+String fmt3(double v) => v.toStringAsFixed(3);
+String fmt6(double v) => v.toStringAsFixed(6);
