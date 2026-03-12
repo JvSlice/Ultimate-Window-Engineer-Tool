9bd524a43eb7721f4a3d65682939595cc6638b09
JvSlice <outeredgeforge@gmail.com>
2026-03-12 09:09:42 -0500

    Update unit_conversions.dart
    fixed an accidental delete

--- lib/conversions/unit_conversions.dart
+++ lib/conversions/unit_conversions.dart
@@ -54,40 +54,40 @@ String performConversion({
     case ConversionTypes.windSpeedToPsf:
       if (direction == Direction.to) {
         final psf = 0.00256 * pow(input, 2);
-        return "${psf.toStringAsFixed(2)} psf";
+        return "${psf.toStringAsFixed(3)} psf";
       } else {
         final mph = sqrt(input / 0.00256);
-        return "${mph.toStringAsFixed(2)} mph";
+        return "${mph.toStringAsFixed(3)} mph";
       }
 
     case ConversionTypes.pressurePsitoPsf:
       if (direction == Direction.to) {
         final psf = input * 144;
-        return "${psf.toStringAsFixed(4)} psf";
+        return "${psf.toStringAsFixed(3)} psf";
       } else {
         final psi = input / 144;
-        return "${psi.toStringAsFixed(4)} psi";
+        return "${psi.toStringAsFixed(3)} psi";
       }
     case ConversionTypes.psftoPa:
       if (direction == Direction.to) {
         final Pa = input * 47.88025898;
-        return "${Pa.toStringAsFixed(4)} Pa";
+        return "${Pa.toStringAsFixed(3)} Pa";
       } else {
         final psf = input / 47.88025898;
-        return "${psf.toStringAsFixed(4)} PSF";
+        return "${psf.toStringAsFixed(3)} PSF";
       }
     case ConversionTypes.CFMperSQfttoCLMperSQm:
       if (direction == Direction.to) {
         final clm = input * 5.08;
-        return "${clm.toStringAsFixed(2)} Cubic litter per minute per sq Meter";
+        return "${clm.toStringAsFixed(3)} Cubic litter per minute per sq Meter";
       } else {
         final cfm = input / 5.08;
-        return "${cfm.toStringAsFixed(2)} CFM per Sq ft";
+        return "${cfm.toStringAsFixed(3)} CFM per Sq ft";
       }
     case ConversionTypes.inchesToCm:
       if (direction == Direction.to) {
         final cm = input * 2.54;
-        return "${cm.toStringAsFixed(4)} cm";
+        return "${cm.toStringAsFixed(3)} cm";
       } else {
         final inches = input / 2.54;
         return "${inches.toStringAsFixed(3)} inches";
@@ -95,44 +95,44 @@ String performConversion({
     case ConversionTypes.feetToInches:
       if (direction == Direction.to) {
         final inches = input * 12;
-        return "${inches.toStringAsFixed(4)} inches";
+        return "${inches.toStringAsFixed(3)} inches";
       } else {
         final feet = input / 12;
-        return "${feet.toStringAsFixed(4)} feet";
+        return "${feet.toStringAsFixed(3)} feet";
       }
     case ConversionTypes.mmToInches:
       if (direction == Direction.to) {
         final inches = input / 25.4;
-        return "${inches.toStringAsFixed(4} inches";
+        return "${inches.toStringAsFixed(3)} inches";
       } else {
         final mm = input * 25.4;
-        return "${mm.toStringAsFixed(4)} mm";
+        return "${mm.toStringAsFixed(3)} mm";
       }
     case ConversionTypes.kgToLbs:
       if (direction == Direction.to) {
         final lbs = input * 2.20462;
-        return "${lbs.toStringAsFixed(4)} lbs";
+        return "${lbs.toStringAsFixed(3)} lbs";
       } else {
         final kg = input / 2.20462;
-        return "${kg.toStringAsFixed(4)} kg";
+        return "${kg.toStringAsFixed(3)} kg";
       }
     case ConversionTypes.forceNToLbsfAndKn:
       if (direction == Direction.to) {
         final lbf = input * 0.224809;
-        return "${lbf.toStringAsFixed(4)} lbsf";
+        return "${lbf.toStringAsFixed(3)} lbsf";
       } else {
         final double newtons = (input >= 1)
             ? (input / 0.224809)
             : (input * 1000);
-        return "${newtons.toStringAsFixed(2)} N";
+        return "${newtons.toStringAsFixed(3)} N";
       }
     case ConversionTypes.literToGallons:
       if (direction == Direction.to) {
         final gallons = input * 0.264172;
-        return "${gallons.toStringAsFixed(4)} gallons";
+        return "${gallons.toStringAsFixed(3)} gallons";
       } else {
         final liters = input / 0.264172;
-        return "${liters.toStringAsFixed(4)} liters";
+        return "${liters.toStringAsFixed(3)} liters";
       }
   }
 }
