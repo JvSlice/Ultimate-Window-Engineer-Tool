d8f64eb72fc242e8c69fc0814a42c53b71b0fd52
JvSlice <outeredgeforge@gmail.com>
2026-04-03 20:52:13 -0500

    Update unit_conversions.dart
    Massive unit conversion add

--- lib/conversions/unit_conversions.dart
+++ lib/conversions/unit_conversions.dart
@@ -1,138 +1,259 @@
 import 'dart:math';
 
-import 'package:flutter/material.dart';
-
-enum ConversionTypes {
-  windSpeedToPsf,
-  pressurePsitoPsf,
-  psftoPa,
-  CFMperSQfttoCLMperSQm,
-  inchesToCm,
-  feetToInches,
-  mmToInches,
-  kgToLbs,
-  forceNToLbsf,
-  literToGallons,
+enum ConversionCategory {
+  pressureAir,
+  length,
+  massForce,
+  volume,
+  temperature,
+  cooking,
 }
 
 enum Direction { to, from }
 
-String conversionLabel(ConversionTypes t) {
-  switch (t) {
-    case ConversionTypes.windSpeedToPsf:
-      return "Wind Speed to PSF";
-    case ConversionTypes.pressurePsitoPsf:
-      return "Pressure PSI to PSF";
-    case ConversionTypes.psftoPa:
-      return "Pressure PSF to PA";
-    case ConversionTypes.CFMperSQfttoCLMperSQm:
-      return "CFM per Sqft to Cubic liters per sqM";
-
-    case ConversionTypes.inchesToCm:
-      return "Inches to Cm";
-    case ConversionTypes.feetToInches:
-      return "Feet to Inches";
-    case ConversionTypes.mmToInches:
-      return "MM to Inches";
-    case ConversionTypes.kgToLbs:
-      return "Kg to LBS";
-    case ConversionTypes.forceNToLbsf:
-      return "N to LBSF";
-    case ConversionTypes.literToGallons:
-      return "L to Gallons";
+String categoryLabel(ConversionCategory c) {
+  switch (c) {
+    case ConversionCategory.pressureAir:
+      return "Pressure / Air";
+    case ConversionCategory.length:
+      return "Length";
+    case ConversionCategory.massForce:
+      return "Mass / Force";
+    case ConversionCategory.volume:
+      return "Volume";
+    case ConversionCategory.temperature:
+      return "Temp";
+    case ConversionCategory.cooking:
+      return "Cooking";
   }
 }
 
-String directionLabel(Direction d) => d == Direction.to ? "To" : "From";
+class ConversionTool {
+  final String label;
+  final ConversionCategory category;
+  final String toUnit;
+  final String fromUnit;
+  final double Function(double input) toFn;
+  final double Function(double input) fromFn;
 
-String performConversion({
-  required ConversionTypes type,
-  required Direction direction,
-  required double input,
-}) {
-  switch (type) {
-    case ConversionTypes.windSpeedToPsf:
-      if (direction == Direction.to) {
-        final psf = 0.00256 * pow(input, 2);
-        return "${psf.toStringAsFixed(3)} psf";
-      } else {
-        final mph = sqrt(input / 0.00256);
-        return "${mph.toStringAsFixed(3)} mph";
-      }
+  const ConversionTool({
+    required this.label,
+    required this.category,
+    required this.toUnit,
+    required this.fromUnit,
+    required this.toFn,
+    required this.fromFn,
+  });
 
-    case ConversionTypes.pressurePsitoPsf:
-      if (direction == Direction.to) {
-        final psf = input * 144;
-        return "${psf.toStringAsFixed(3)} psf";
-      } else {
-        final psi = input / 144;
-        return "${psi.toStringAsFixed(3)} psi";
-      }
-    case ConversionTypes.psftoPa:
-      if (direction == Direction.to) {
-        final Pa = input * 47.88025898;
-        return "${Pa.toStringAsFixed(3)} Pa";
-      } else {
-        final psf = input / 47.88025898;
-        return "${psf.toStringAsFixed(3)} PSF";
-      }
-    case ConversionTypes.CFMperSQfttoCLMperSQm:
-      if (direction == Direction.to) {
-        final clm = input * 5.08;
-        return "${clm.toStringAsFixed(3)} Cubic litter per minute per sq Meter";
-      } else {
-        final cfm = input / 5.08;
-        return "${cfm.toStringAsFixed(3)} CFM per Sq ft";
-      }
-    case ConversionTypes.inchesToCm:
-      if (direction == Direction.to) {
-        final cm = input * 2.54;
-        return "${cm.toStringAsFixed(3)} cm";
-      } else {
-        final inches = input / 2.54;
-        return "${inches.toStringAsFixed(3)} inches";
-      }
-    case ConversionTypes.feetToInches:
-      if (direction == Direction.to) {
-        final inches = input * 12;
-        return "${inches.toStringAsFixed(3)} inches";
-      } else {
-        final feet = input / 12;
-        return "${feet.toStringAsFixed(3)} feet";
-      }
-    case ConversionTypes.mmToInches:
-      if (direction == Direction.to) {
-        final inches = input / 25.4;
-        return "${inches.toStringAsFixed(3)} inches";
-      } else {
-        final mm = input * 25.4;
-        return "${mm.toStringAsFixed(3)} mm";
-      }
-    case ConversionTypes.kgToLbs:
-      if (direction == Direction.to) {
-        final lbs = input * 2.20462;
-        return "${lbs.toStringAsFixed(3)} lbs";
-      } else {
-        final kg = input / 2.20462;
-        return "${kg.toStringAsFixed(3)} kg";
-      }
-    case ConversionTypes.forceNToLbsf:
-      if (direction == Direction.to) {
-        final lbf = input * 0.224809;
-        return "${lbf.toStringAsFixed(3)} lbsf";
-      } else {
-        final double newtons = (input >= 1)
-            ? (input / 0.224809)
-            : (input * 1000);
-        return "${newtons.toStringAsFixed(3)} N";
-      }
-    case ConversionTypes.literToGallons:
-      if (direction == Direction.to) {
-        final gallons = input * 0.264172;
-        return "${gallons.toStringAsFixed(3)} gallons";
-      } else {
-        final liters = input / 0.264172;
-        return "${liters.toStringAsFixed(3)} liters";
-      }
+  String convert(Direction direction, double input) {
+    final value = direction == Direction.to ? toFn(input) : fromFn(input);
+    final unit = direction == Direction.to ? toUnit : fromUnit;
+    return "${value.toStringAsFixed(3)} $unit";
   }
 }
+
+final List<ConversionTool> conversionTools = [
+  // Pressure / Air
+  ConversionTool(
+    label: "Wind Speed ↔ PSF",
+    category: ConversionCategory.pressureAir,
+    toUnit: "psf",
+    fromUnit: "mph",
+    toFn: (input) => 0.00256 * pow(input, 2).toDouble(),
+    fromFn: (input) => sqrt(input / 0.00256),
+  ),
+  ConversionTool(
+    label: "PSI ↔ PSF",
+    category: ConversionCategory.pressureAir,
+    toUnit: "psf",
+    fromUnit: "psi",
+    toFn: (input) => input * 144,
+    fromFn: (input) => input / 144,
+  ),
+  ConversionTool(
+    label: "PSF ↔ Pa",
+    category: ConversionCategory.pressureAir,
+    toUnit: "Pa",
+    fromUnit: "psf",
+    toFn: (input) => input * 47.88025898,
+    fromFn: (input) => input / 47.88025898,
+  ),
+  ConversionTool(
+    label: "CFM/ft² ↔ L/min/m²",
+    category: ConversionCategory.pressureAir,
+    toUnit: "L/min/m²",
+    fromUnit: "CFM/ft²",
+    toFn: (input) => input * 5.08,
+    fromFn: (input) => input / 5.08,
+  ),
+
+  // Length
+  ConversionTool(
+    label: "Inches ↔ Centimeters",
+    category: ConversionCategory.length,
+    toUnit: "cm",
+    fromUnit: "in",
+    toFn: (input) => input * 2.54,
+    fromFn: (input) => input / 2.54,
+  ),
+  ConversionTool(
+    label: "Feet ↔ Inches",
+    category: ConversionCategory.length,
+    toUnit: "in",
+    fromUnit: "ft",
+    toFn: (input) => input * 12,
+    fromFn: (input) => input / 12,
+  ),
+  ConversionTool(
+    label: "Millimeters ↔ Inches",
+    category: ConversionCategory.length,
+    toUnit: "in",
+    fromUnit: "mm",
+    toFn: (input) => input / 25.4,
+    fromFn: (input) => input * 25.4,
+  ),
+  ConversionTool(
+    label: "Feet ↔ Meters",
+    category: ConversionCategory.length,
+    toUnit: "m",
+    fromUnit: "ft",
+    toFn: (input) => input * 0.3048,
+    fromFn: (input) => input / 0.3048,
+  ),
+  ConversionTool(
+    label: "Meters ↔ Yards",
+    category: ConversionCategory.length,
+    toUnit: "yd",
+    fromUnit: "m",
+    toFn: (input) => input * 1.09361,
+    fromFn: (input) => input / 1.09361,
+  ),
+
+  // Mass / Force
+  ConversionTool(
+    label: "Kilograms ↔ Pounds",
+    category: ConversionCategory.massForce,
+    toUnit: "lb",
+    fromUnit: "kg",
+    toFn: (input) => input * 2.20462,
+    fromFn: (input) => input / 2.20462,
+  ),
+  ConversionTool(
+    label: "Newtons ↔ lbf",
+    category: ConversionCategory.massForce,
+    toUnit: "lbf",
+    fromUnit: "N",
+    toFn: (input) => input * 0.224809,
+    fromFn: (input) => input / 0.224809,
+  ),
+  ConversionTool(
+    label: "kN ↔ lbf",
+    category: ConversionCategory.massForce,
+    toUnit: "lbf",
+    fromUnit: "kN",
+    toFn: (input) => input * 224.809,
+    fromFn: (input) => input / 224.809,
+  ),
+  ConversionTool(
+    label: "Grams ↔ Ounces",
+    category: ConversionCategory.massForce,
+    toUnit: "oz",
+    fromUnit: "g",
+    toFn: (input) => input * 0.035274,
+    fromFn: (input) => input / 0.035274,
+  ),
+
+  // Volume
+  ConversionTool(
+    label: "Liters ↔ Gallons",
+    category: ConversionCategory.volume,
+    toUnit: "gal",
+    fromUnit: "L",
+    toFn: (input) => input * 0.264172,
+    fromFn: (input) => input / 0.264172,
+  ),
+  ConversionTool(
+    label: "Milliliters ↔ Fluid Ounces",
+    category: ConversionCategory.volume,
+    toUnit: "fl oz",
+    fromUnit: "mL",
+    toFn: (input) => input * 0.033814,
+    fromFn: (input) => input / 0.033814,
+  ),
+  ConversionTool(
+    label: "Cubic Inches ↔ Liters",
+    category: ConversionCategory.volume,
+    toUnit: "L",
+    fromUnit: "in³",
+    toFn: (input) => input * 0.0163871,
+    fromFn: (input) => input / 0.0163871,
+  ),
+  ConversionTool(
+    label: "Cubic Feet ↔ Cubic Meters",
+    category: ConversionCategory.volume,
+    toUnit: "m³",
+    fromUnit: "ft³",
+    toFn: (input) => input * 0.0283168,
+    fromFn: (input) => input / 0.0283168,
+  ),
+
+  // Temperature
+  ConversionTool(
+    label: "°F ↔ °C",
+    category: ConversionCategory.temperature,
+    toUnit: "°C",
+    fromUnit: "°F",
+    toFn: (input) => (input - 32) * 5 / 9,
+    fromFn: (input) => (input * 9 / 5) + 32,
+  ),
+  ConversionTool(
+    label: "°C ↔ K",
+    category: ConversionCategory.temperature,
+    toUnit: "K",
+    fromUnit: "°C",
+    toFn: (input) => input + 273.15,
+    fromFn: (input) => input - 273.15,
+  ),
+
+  // Cooking
+  ConversionTool(
+    label: "Teaspoons ↔ Milliliters",
+    category: ConversionCategory.cooking,
+    toUnit: "mL",
+    fromUnit: "tsp",
+    toFn: (input) => input * 4.92892,
+    fromFn: (input) => input / 4.92892,
+  ),
+  ConversionTool(
+    label: "Tablespoons ↔ Milliliters",
+    category: ConversionCategory.cooking,
+    toUnit: "mL",
+    fromUnit: "tbsp",
+    toFn: (input) => input * 14.7868,
+    fromFn: (input) => input / 14.7868,
+  ),
+  ConversionTool(
+    label: "Cups ↔ Milliliters",
+    category: ConversionCategory.cooking,
+    toUnit: "mL",
+    fromUnit: "cups",
+    toFn: (input) => input * 236.588,
+    fromFn: (input) => input / 236.588,
+  ),
+  ConversionTool(
+    label: "Pints ↔ Liters",
+    category: ConversionCategory.cooking,
+    toUnit: "L",
+    fromUnit: "pt",
+    toFn: (input) => input * 0.473176,
+    fromFn: (input) => input / 0.473176,
+  ),
+  ConversionTool(
+    label: "Quarts ↔ Liters",
+    category: ConversionCategory.cooking,
+    toUnit: "L",
+    fromUnit: "qt",
+    toFn: (input) => input * 0.946353,
+    fromFn: (input) => input / 0.946353,
+  ),
+];
