94191edba7bb6a158513dc931151e5d6e0448c1a
JvSlice <outeredgeforge.com>
2026-04-07 17:44:19 -0500

    removing hello pi issues

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_pi/main.dart';
+import 'package:ultimate_window_engineer_tool/main.dart';
+import 'package:ultimate_window_engineer_tool/app_theme.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(themeController: AppThemeController()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
